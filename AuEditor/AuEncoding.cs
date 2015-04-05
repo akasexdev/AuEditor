62f4a77f38578772d1b2d305f5d9cc870b9cca6a
akasexdev <akasexdev@gmail.com>
2015-04-05 13:25:55 -0400

    Added suppoerted Encoding types

---
+++ AuEditor/AuEncoding.cs
@@ -0,0 +1,11 @@
+﻿namespace AuEditor
+{
+    public enum AuEncoding : uint
+    {
+        None = 0,
+        Linear8Bit = 2,
+        Linear16Bit = 3,
+        Linear24Bit = 4,
+        Linear32Bit = 5
+    }
+}
